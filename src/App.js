ab1e1d498e63ccacbdc207eb87fafa95c416bd0f
anatoli <johndoe@example.com>
2024-03-15 10:41:07 +0400

    pushed react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      orders: [],
+      items:[
+        {
+          id: 1,
+          title: 'iphone15Pro',
+          img: 'iphone.webp',
+          desc: 'vacarvum e',
+          category: 'iphone',
+          price: '1000'
+        },
+        {
+          id: 2,
+          title: 'mackbookAirM1',
+          img: 'macbook.webp',
+          desc: 'vacarvum e',
+          category: 'mackbook',
+          price: '1050'
+        },
+        {
+          id: 3,
+          title: 'tablets',
+          img: 'tablet.jpeg',
+          desc: 'vacarvum e',
+          category: 'tablet',
+          price: '1150'
+        }
+
+      ]
+    }
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
+
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
+      <Items items={this.state.items} onAdd={this.addToOrder} />
+      <Footer />
     </div>
   );
+}
+
+deleteOrder(id){
+this.setState({orders: this.state.orders.filter(el => el.id !==id)})
+}
+
+addToOrder(item){
+  let isInArray = false
+this.state.orders.forEach(el => { 
+  if(el.id === item.id)
+  isInArray= true
+})
+if(!isInArray)
+this.setState({orders: [...this.state.orders, item]} )
+}
+
 }
 
 export default App;
